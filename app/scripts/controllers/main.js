1086bb80dbbdec8a85876d4a914063f70e36d875
Jordan Braginton <jordan.braginton@gmail.com>
2014-03-19 12:28:01 -0600

    added in the beginning of a blog functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('babynamerApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $firebase) {
   	document.body.style.background = 'lightgreen';
+  	 var threadRef = new Firebase('https://babynamer.firebaseIO.com/threads');
+    $scope.threads = $firebase(threadRef);
+    $scope.addThread = function(e){
+    	if(e.keyCode !== 13) {
+    		return;
+    	}
+    	$scope.threads.$add({
+    		body: $scope.thread
+    	});
+    	$scope.thread = '';
+    };
   });
